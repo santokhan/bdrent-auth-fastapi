49e4b1b53253c2f5c3a2b811897c439fd4c4b92a
santokhan <santokhan1999@gmail.com>
2024-11-24 18:55:25 +0600

    update env

--- setup.sh
+++ setup.sh
@@ -4,11 +4,3 @@ exit
 pip install fastapi uvicorn python-dotenv pymongo pydantic[email] phonenumbers argon2-cffi PyJWT motor
 
 pip freeze > requirements.txt
-pip install -r requirements.txt
-
-source venv/Scripts/activate
-uvicorn main:app --port 3005 --reload
-
-venv\Scripts\activate
-uvicorn main:app --port 3005 --reload
-
